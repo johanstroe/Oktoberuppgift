664cdcb1a36471f03f8d766e8dfb1f62fe9197f6
Johan Stroeberg <johanstroberg91@gmail.com>
2024-09-13 11:50:37 +0200

    styling av sista sidan, inte helt klart

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+function myDropFunc() {
+  var x = document.getElementById("demoDrop");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+
 function darkmodetoggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
