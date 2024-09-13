7bbd25aeeeecbd0d996c5aa08147200f0ca0a4a3
Johan Stroeberg <johanstroberg91@gmail.com>
2024-09-13 16:22:47 +0200

    fortsätting på styling, gjort en accordion som inte är klar

--- javascript.js
+++ javascript.js
@@ -1,17 +1,21 @@
-function myDropFunc() {
-  var x = document.getElementById("demoDrop");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-    x.previousElementSibling.className += " w3-green";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-    x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace(" w3-green", "");
-  }
-}
-
 
 function darkmodetoggle() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
